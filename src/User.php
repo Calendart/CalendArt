1189a5909929698f78b411f54b239e7b826684b1
Baptiste Clavié <clavie.b@gmail.com>
2014-08-14 15:49:52 +0200

    Add User object

---
+++ src/User.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the Calendar package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace Calendar;
+
+/**
+ * Represents a User
+ *
+ * This class should be extended by the different adapters to specify their
+ * needs ; it should hydrate its descendants
+ *
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class User
+{
+    /** @var string User (unique) hash */
+    protected $id;
+
+    /** @var string User's name */
+    protected $name;
+
+    /** @var string User's email */
+    protected $email;
+
+    public function __construct($name, $email)
+    {
+        $this->name  = $name;
+        $this->email = $email;
+
+        $this->id = sha1($email . $name);
+    }
+
+    /** @return string */
+    final public function getId()
+    {
+        return $this->id;
+    }
+
+    /** @return string */
+    final public function getName()
+    {
+        return $this->name;
+    }
+
+    /** @return string */
+    final public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
