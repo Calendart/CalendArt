f26ab90f2795485ad18ee0050cb3c67c9467fa67
Baptiste Clavié <clavie.b@gmail.com>
2014-08-18 15:08:28 +0200

    Add Event object

---
+++ src/Event.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * This file is part of the Calendar package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace Calendar;
+
+use Datetime,
+    InvalidArgumentException;
+
+/**
+ * Represents an Event in a Calendar
+ *
+ * Like all generic objects, this object should be extended by the adapter
+ *
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class Event
+{
+    /** @var Datetime Start date of this event */
+    protected $start;
+
+    /** @var Datetime End date of this event */
+    protected $end;
+
+    /** @var string name of this event */
+    protected $name;
+
+    /** @var string Description of this event */
+    protected $description;
+
+    /** @var User owner of this event */
+    protected $owner;
+
+    public function __construct(User $owner, $name, Datetime $start, Datetime $end)
+    {
+        $this->name  = $name;
+        $this->owner = $owner;
+
+        if ($start > $end) {
+            throw new InvalidArgumentException('An event cannot start after it was ended');
+        }
+
+        $this->end   = $end;
+        $this->start = $start;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /** @return $this */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /** @return $this */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /** @return User */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /** @return Datetime */
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    /** @return $this */
+    public function setStart(Datetime $start)
+    {
+        if ($this->end < $start) {
+            throw new InvalidArgumentException('An event cannot start after it was ended');
+        }
+
+        $this->start = $start;
+
+        return $this;
+    }
+
+    /** @return Datetime */
+    public function getEnd()
+    {
+        return $this->end;
+    }
+
+    /** @return $this */
+    public function setEnd(Datetime $end)
+    {
+        if ($this->start > $end) {
+            throw new InvalidArgumentException('An event cannot end before it was started');
+        }
+
+        $this->end = $end;
+
+        return $this;
+    }
+
+    /**
+     * Checks if this event has already ended
+     *
+     * @return Boolean
+     */
+    public function hasEnded(Datetime $current = null)
+    {
+        return $this->end < ($current ?: new Datetime);
+    }
+
+    /**
+     * Checks if this event has already started
+     *
+     * @return Boolean
+     */
+    public function hasStarted(Datetime $current = null)
+    {
+        return $this->start <= ($current ?: new Datetime);
+    }
+
+    /**
+     * Checks if this event is currently running
+     *
+     * @return Boolean
+     */
+    public function isRunning(Datetime $current = null)
+    {
+        $current = $current ?: new Datetime;
+
+        return $this->hasStarted($current) && !$this->hasEnded($current);
+    }
+}
+
