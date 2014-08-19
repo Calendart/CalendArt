7447ae0314939b6ba05cf4fc5cf341b3f3c7b1dc
Baptiste Clavié <clavie.b@gmail.com>
2014-08-19 17:21:43 +0200

    Change the package name to its new found name : CalendArt
    Courtesy of @guillaumepotier ; For any complaints, please check with
    him. <3

--- src/EventParticipation.php
+++ src/EventParticipation.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Calendar package
+ * This file is part of the CalendArt package
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
@@ -9,7 +9,7 @@
  * @license   http://www.opensource.org/licenses/MIT-License MIT License
  */
 
-namespace Calendar;
+namespace CalendArt;
 
 use Datetime,
     InvalidArgumentException;
