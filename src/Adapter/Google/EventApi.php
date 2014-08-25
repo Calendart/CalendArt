3592ade9cf6a111313a89a5b3d921cf4ae4ba514
Baptiste Clavié <clavie.b@gmail.com>
2014-08-25 14:30:34 +0200

    [Google] Refactor the query handling in the Event api

--- src/Adapter/Google/EventApi.php
+++ src/Adapter/Google/EventApi.php
@@ -31,6 +31,8 @@ class EventApi implements EventApiInterface
     /** @var Calendar */
     private $calendar;
 
+    protected static $query = ['fields' => 'attendees(displayName,email),created,creator(displayName,email),description,end,id,location,start,status,summary,updated'];
+
     public function __construct(Guzzle $client, Calendar $calendar)
     {
         $this->guzzle   = $client;
@@ -41,15 +43,18 @@ class EventApi implements EventApiInterface
     public function getList()
     {
         $nextPageToken = null;
+        $query         = static::$query;
         $list          = new ArrayCollection;
 
-        do {
-            $query = ['fields' => 'items(attendees(displayName,email,id),created,creator(displayName,email),description,end,etag,iCalUID,id,location,start,status,summary),nextPageToken,nextSyncToken'];
+        if (null !== $this->calendar->getSyncToken()) {
+            $query['nextSyncToken'] = $this->calendar->getSyncToken();
+        }
 
+        $query['fields'] = sprintf('items(%s),nextSyncToken,nextPageToken', $query['fields']);
+
+        do {
             if (null !== $nextPageToken) {
                 $query['nextPageToken'] = $nextPageToken;
-            } elseif (null !== $this->calendar->getSyncToken()) {
-                $query['nextSyncToken'] = $this->calendar->getSyncToken();
             }
 
             $response = $this->guzzle->get(sprintf('/calendars/%s/events', $this->calendar->getId()), ['query' => $query]);
@@ -61,7 +66,7 @@ class EventApi implements EventApiInterface
             $result = $response->json();
 
             foreach ($result['items'] as $item) {
-                $list[$item['id']] = Event::hydrate($item);
+                $list[$item['id']] = Event::hydrate($this->calendar, $item);
             }
 
             $nextPageToken = isset($result['nextPageToken']) ? $result['nextPageToken'] : null;
@@ -75,13 +80,13 @@ class EventApi implements EventApiInterface
     /** {@inheritDoc} */
     public function get($identifier)
     {
-        $response = $this->guzzle->get(sprintf('/calendars/%s/events/%s', $this->calendar->getId(), $identifier), ['query' => ['fields' => 'attendees,created,description,end,id,kind,location,organizer,recurrence,sequence,start,summary']]);
+        $response = $this->guzzle->get(sprintf('/calendars/%s/events/%s', $this->calendar->getId(), $identifier), ['query' => static::$query]);
 
         if (200 > $response->getStatusCode() || 300 <= $response->getStatusCode()) {
             throw new ApiErrorException($response);
         }
 
-        return Event::hydrate($response->json());
+        return Event::hydrate($this->calendar, $response->json());
     }
 }
 
