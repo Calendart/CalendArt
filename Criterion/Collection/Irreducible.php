82d44591860cf016d501becc71e71c848aa27948
Baptiste Clavié <clavie.b@gmail.com>
2014-09-02 15:02:48 +0200

    Rename Query criterion to Collection criterion

---
+++ Criterion/Collection/Irreducible.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the CalendArt package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace CalendArt\Adapter\Google\Criterion\Collection;
+
+/**
+ * Represents an irreducible element for a Collection criterion
+ *
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class Irreducible extends Element
+{
+}
+
