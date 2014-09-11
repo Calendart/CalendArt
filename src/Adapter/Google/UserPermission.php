88bb6ff9664a4adb56486cf55f6fb8d7ea2bb84b
Baptiste Clavié <clavie.b@gmail.com>
2014-09-11 17:40:47 +0200

    [Google] Extends the UserPermission

---
+++ src/Adapter/Google/UserPermission.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the CalendArt package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace CalendArt\Google;
+
+use InvalidArgumentException;
+
+use CalendArt\UserPermission as Base;
+
+/**
+ * Represents a UserPermission from a Google point of view
+ *
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class UserPermission extends Base
+{
+    public static function hydrate(Calendar $calendar, User $user, $role)
+    {
+        return new static($calendar, $user, self::translateRole($role));
+    }
+
+    private static function translateRole($role)
+    {
+        $flags = parent::NOPE;
+
+        switch ($role) {
+            case 'owner':
+            case 'writer':
+                $flags |= parent::WRITE;
+
+            case 'reader':
+            case 'freeBusyReader':
+                $flags |= parent::READ;
+
+            case 'none': break;
+
+            default:
+                throw new InvalidArgumentException(sprintf('Role "%s" not recognized', $role));
+        }
+
+        return $flag;
+    }
+}
+
